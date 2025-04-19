93bf27bf6c75f8e4b5f436ac578fff5eb55facc1
ElliX <codeellix@gmail.com>
2025-04-19 19:05:26 +0200

    feat: component data moved to servicesData

---
+++ clinic/src/app/all-services/page.jsx
@@ -0,0 +1,11 @@
+import styles from './all-services.module.css';
+
+const AllServices = () => {
+    return (
+        <div className={styles.wrap}>
+         
+        </div>
+    )
+}
+
+export default AllServices;
