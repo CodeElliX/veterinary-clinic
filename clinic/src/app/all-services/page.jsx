99329c23b7ac0b2089504dacd73fea9785940740
ElliX <codeellix@gmail.com>
2025-05-04 20:29:03 +0300

    feat: style Surgery directions section

--- clinic/src/app/all-services/page.jsx
+++ clinic/src/app/all-services/page.jsx
@@ -1,7 +1,23 @@
 import styles from './all-services.module.css';
 import { allCards } from '../data/servicesData';
+import Link from 'next/link';
 
 const AllServices = () => {
+
+    const titleToPath = {
+        'Хірургічні маніпуляції': 'surgery',
+        'МРТ та Компьютерна томографія': 'MRI',
+        'Стаціонар: післяопераційний та інфекційний': 'hospital',
+        'Експертиза по діагностіці ТБС': 'diagnosticsTBS',
+        'Документи для вивозу тварин за кордон': 'documents',
+        'УЗД абдомінальне': 'ultrasound',
+        'Лабораторні дослідження': 'laboratory',
+        'Кардіологічне обстеження': 'cardiology',
+        'Ендоскопія': 'endoscopy',
+        'Онкологія': 'oncology',
+        'Неврологія': 'neurology',
+    };
+
     return (
         <div className={styles.wrap}>
             <div className={styles.description}>
@@ -10,10 +26,15 @@ const AllServices = () => {
             </div>
             <div className={styles.services_cards}>
                 {allCards.map((el, index) => {
-                    return <div key={index} className={styles.card}>
-                        {el.photo}
-                        <h2>{el.tittle}</h2>
-                    </div>
+                    const path = titleToPath[el.tittle]
+                    return (
+                        <Link href={`${path}`}  key={index} className={styles.card}>
+                            <div>
+                                {el.photo}
+                                <h2>{el.tittle}</h2>
+                            </div>
+                        </Link>
+                    )
                 })}
             </div>
         </div>
