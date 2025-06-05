a3004cf1893138892b70035d0b999bbc5b65cf09
ElliX <codeellix@gmail.com>
2025-06-05 22:09:15 +0300

    style: made AllServices component mobile-friendly

--- clinic/src/app/all-services/page.jsx
+++ clinic/src/app/all-services/page.jsx
@@ -1,7 +1,9 @@
+'use client'
 import styles from './all-services.module.css';
 import { allCards } from '../data/servicesData';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useEffect } from 'react';
 
 const AllServices = () => {
 
@@ -19,6 +21,28 @@ const AllServices = () => {
         'Неврологія': 'neurology',
     };
 
+    useEffect(() => {
+        if (window.innerWidth <= 1174) {
+            const observer = new IntersectionObserver(
+                (entries) => {
+                    entries.forEach((entry) => {
+                        if (entry.isIntersecting) {
+                            entry.target.classList.add(styles.mobile_animate);
+                        } else {
+                            entry.target.classList.remove(styles.mobile_animate);
+                        }
+                    });
+                },
+                { threshold: 0.8 }
+            );
+
+            const cards = document.querySelectorAll(`.${styles.card}`);
+            cards.forEach((card) => observer.observe(card));
+
+            return () => observer.disconnect();
+        }
+    }, []);
+
     return (
         <div className={styles.wrap}>
             <div className={styles.description}>
@@ -29,10 +53,10 @@ const AllServices = () => {
                 {allCards.map((el, index) => {
                     const path = titleToPath[el.tittle]
                     return (
-                        <Link href={`${path}`}  key={index} className={styles.card}>
+                        <Link href={`${path}`} key={index} className={styles.card}>
                             <Image src={`${el.back}`} width={415} height={510} alt='cards' />
-                                {el.photo}
-                                <h2>{el.tittle}</h2>
+                            {el.photo}
+                            <h2>{el.tittle}</h2>
                         </Link>
                     )
                 })}
